fdf95bb5738d503a470098a9b0bfc359e5e837bf
Vapour101 <vapour101@gmail.com>
2017-08-09 13:43:44 +0200

    Apparently JavaFX doesn't like the Main class to be inside a package for some reason...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,8 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package ui;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
