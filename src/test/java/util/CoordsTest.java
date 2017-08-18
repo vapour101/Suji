214e6a78dfd8ceaed53f4f5909faf753b919a966
Vapour101 <vapour101@gmail.com>
2017-08-18 20:54:14 +0200

    More tests for Coords

--- src/test/java/util/CoordsTest.java
+++ src/test/java/util/CoordsTest.java
@@ -17,13 +17,27 @@
 
 package util;
 
+import javafx.util.Pair;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static util.Coords.getCoords;
 
 public class CoordsTest {
+
+    @Test
+    public void equalTo() {
+        Coords c1 = new Coords(4,3);
+        Coords c2 = new Coords(4,3);
+        Pair<Integer, Integer> notCoords = new Pair<>(4, 3);
+
+        assertThat(c1.equals(c2), is(true));
+        assertThat(c2.equals(c1), is(true));
+        assertThat(c1.equals(c1), is(true));
+        assertThat(c1.equals(notCoords), is(false));
+    }
 
     @Test
     public void coordsGetter() {
@@ -35,5 +49,30 @@ public class CoordsTest {
     @Test
     public void convertToString() {
         assertThat(getCoords(3, 4).toString(), is("(3, 4)"));
+    }
+
+    @Test
+    public void neighbours()
+    {
+        Coords center = getCoords(10,10);
+        Coords topLeft = getCoords(1,1);
+        Coords bottomRight = getCoords(19,19);
+
+        assertThat(center.getNeighbours().size(), is(4));
+        assertThat(center.getNeighbours(), hasItems(
+                getCoords(10,9),
+                getCoords(9,10),
+                getCoords(10,11),
+                getCoords(11,10)));
+
+        assertThat(topLeft.getNeighbours().size(), is(2));
+        assertThat(topLeft.getNeighbours(), hasItems(
+                getCoords(1,2),
+                getCoords(2,1)));
+
+        assertThat(bottomRight.getNeighbours().size(), is(2));
+        assertThat(bottomRight.getNeighbours(), hasItems(
+                getCoords(19,18),
+                getCoords(18,19)));
     }
 }
